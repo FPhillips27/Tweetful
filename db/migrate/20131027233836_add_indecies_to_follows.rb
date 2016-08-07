fa698ccdaa1c5cda081b5bc91f008617cf9c554a
Forrest <mrforrestphillips@gmail.com>
2016-08-07 01:02:28 +0000

    Addressing Deprecation Warnings in db:migrate files

--- db/migrate/20131027233836_add_indecies_to_follows.rb
+++ db/migrate/20131027233836_add_indecies_to_follows.rb
@@ -1,4 +1,4 @@
-class AddIndeciesToFollows < ActiveRecord::Migration
+class AddIndeciesToFollows < ActiveRecord::Migration[4.2]
   def change
     add_index :follows, [:user_id, :following_id]
   end
